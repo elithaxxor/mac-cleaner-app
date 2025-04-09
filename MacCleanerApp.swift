3558b1a811b418ade2659e0880a27527fda3d654
adel <86808805+elithaxxor@users.noreply.github.com>
2025-04-09 06:51:02 -0400

    Create  MacCleanerApp.swift

---
+++ MacCleanerApp.swift
@@ -0,0 +1,12 @@
+/* the entry point of the SwiftUI app. Save it as MacCleanerApp.swift in your main project directory */
+import SwiftUI
+
+@main
+struct MacCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
